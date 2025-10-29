cabdcff201adf915a137ae5a3686b22d61dd71be
hismyhill <owolabismaeel@gmail.com>
2025-10-29 01:59:51 +0100

    Updated build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "url";
-import path from "path";
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/vue/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
