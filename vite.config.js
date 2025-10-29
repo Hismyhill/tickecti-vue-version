26cd6c08923919a97d2786dfa82d288668fc94e9
hismyhill <owolabismaeel@gmail.com>
2025-10-29 09:31:15 +0100

    Updated  vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "url";
+
 export default defineConfig({
-  base: "/vue/",
+  base: "/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@shared": fileURLToPath(new URL("../shared-assets", import.meta.url)),
+      // "@shared": fileURLToPath(new URL("../shared-assets", import.meta.url)),
     },
   },
   build: {
